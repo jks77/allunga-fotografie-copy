6d31f4709d153abec38e096deee699984d827fdd
jks77 <jankrijn@gmail.com>
2020-06-08 07:12:17 +0200

    fixed hamburger, project finished

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const ul = document.querySelector('ul');
+const lineOne = document.getElementById('line-1');
+const lineTwo = document.getElementById('line-2');
+const lineThree = document.getElementById('line-3');
+
 
 hamburger.addEventListener('click', () => {
 	ul.classList.toggle('nav-links');
+	lineOne.classList.toggle('rotate-45');
+	lineTwo.classList.toggle('display-none');
+	lineThree.classList.toggle('rotate-minus-45');
 });
