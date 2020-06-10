2d8bad610d4fe1b8c7985f68c2603b0059fd5c8f
jks77 <jankrijn@gmail.com>
2020-06-10 08:31:44 +0200

    fixed bug navbar landscape

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const ul = document.querySelector('ul');
 const lineOne = document.getElementById('line-1');
 const lineTwo = document.getElementById('line-2');
 const lineThree = document.getElementById('line-3');
-
+let open = false;
 
 hamburger.addEventListener('click', () => {
-	ul.classList.toggle('nav-links');
+	if (!open) {
+		ul.classList.add('open-hamburger');
+		open = true;
+	} else {
+		ul.classList.remove('open-hamburger');
+		open = false;
+	}
 	lineOne.classList.toggle('rotate-45');
 	lineTwo.classList.toggle('display-none');
 	lineThree.classList.toggle('rotate-minus-45');
